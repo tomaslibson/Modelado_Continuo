d8a956ac4ee26b047705d785257f055ece1e640c
tomaslibson <libsontomas010@gmail.com>
2026-04-25 15:05:56 -0300

    ej 2 y 3

--- testej3.py
+++ testej3.py
@@ -6,7 +6,7 @@ alpha = 1
 
 # Campo vectorial
 def f(u, v):
-    return v, -u + 1/alpha + 0.005 * u**2
+    return v, -u + 1/alpha + 0.05 * u**2
 
 # Sistema para odeint
 def derivadas_para_odeint(uv, theta):
@@ -14,8 +14,6 @@ def derivadas_para_odeint(uv, theta):
     du_dtheta, dv_dtheta = f(u, v)
     return du_dtheta, dv_dtheta
 
-fig, ax = plt.subplots()
-ax.set_aspect(1.0)
 
 # Intervalo angular (varias vueltas)
 ti = 0.0
@@ -23,35 +21,59 @@ tf = 8*np.pi   # 4 vueltas
 nt = 2000
 lt = np.linspace(ti, tf, nt)
 
-# Distintos valores de excentricidad
-epsilons = [0.2, 0.5, 0.8, 1, 1.2]
+# 0<= eps < 1
+epsilons1 = [0.2, 0.5, 0.8]
 
-for eps in epsilons:
-    
-    # Condiciones iniciales
+fig, ax = plt.subplots()
+ax.set_aspect(1.0)
+
+for eps in epsilons1:
     u0 = (1 + eps)/alpha
     v0 = 0
     estado_inicial = [u0, v0]
-    
-    # Resolver sistema
+
     sol = odeint(derivadas_para_odeint, estado_inicial, lt)
-    
     u, v = sol.T
     r = 1/u
-    
-    # Pasaje a coordenadas cartesianas
+
     x = r * np.cos(lt)
     y = r * np.sin(lt)
-    
+
     plt.plot(x, y, label=f"ε = {eps}")
 
-# Sol en el origen
 plt.scatter(0, 0, color='orange', label='Sol')
-
 plt.xlim(-5, 5)
 plt.ylim(-5, 5)
-
 plt.grid()
 plt.legend()
-plt.title("Órbitas de Kepler (δ = 0)")
+plt.title("Órbitas (ε < 1, δ = 0.05)")
+plt.show()
+
+
+# eps >= 1
+epsilons2 = [1, 1.2]
+
+fig, ax = plt.subplots()
+ax.set_aspect(1.0)
+
+for eps in epsilons2:
+    u0 = (1 + eps)/alpha
+    v0 = 0
+    estado_inicial = [u0, v0]
+
+    sol = odeint(derivadas_para_odeint, estado_inicial, lt)
+    u, v = sol.T
+    r = 1/u
+
+    x = r * np.cos(lt)
+    y = r * np.sin(lt)
+
+    plt.plot(x, y, label=f"ε = {eps}")
+
+plt.scatter(0, 0, color='orange', label='Sol')
+plt.xlim(-5, 5)
+plt.ylim(-5, 5)
+plt.grid()
+plt.legend()
+plt.title("Órbitas (ε ≥ 1, δ = 0.05)")
 plt.show()
